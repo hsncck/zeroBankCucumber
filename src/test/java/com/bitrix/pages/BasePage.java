2c8c5527a426b7a4fdab8f4f82f05cd57436d202
merkurt2001 <merkurt2001@gmail.com>
2021-02-04 12:46:26 +0300

    Completed Version

--- src/test/java/com/bitrix/pages/BasePage.java
+++ src/test/java/com/bitrix/pages/BasePage.java
@@ -42,72 +42,15 @@ public abstract class  BasePage {
     public WebElement logOutLink;
 
 
-
     public BasePage() {
         PageFactory.initElements(Driver.get(), this);
     }
 
 
-//    /**
-//     * @return page name, for example: Dashboard
-//     */
-//    public String getPageSubTitle() {
-//        //ant time we are verifying page name, or page subtitle, loader mask appears
-//        waitUntilLoaderScreenDisappear();
-////        BrowserUtils.waitForStaleElement(pageSubTitle);
-//        return pageSubTitle.getText();
-//    }
-//
-//
-//
-//
-//    public String getUserName(){
-//        waitUntilLoaderScreenDisappear();
-//        BrowserUtils.waitForVisibility(userName, 5);
-//        return userName.getText();
-//    }
-//
-//
-//
     public void logOut(){
         BrowserUtils.waitFor(2);
         BrowserUtils.clickWithJS(userName);
         BrowserUtils.clickWithJS(logOutLink);
     }
-//    public void goToMyUser(){
-//        waitUntilLoaderScreenDisappear();
-//        BrowserUtils.waitForClickablility(userName, 5).click();
-//        BrowserUtils.waitForClickablility(myUser, 5).click();
-//
-//    }
-//
-//    /**
-//     * This method will navigate user to the specific module in vytrack application.
-//     * For example: if tab is equals to Activities, and module equals to Calls,
-//     * Then method will navigate user to this page: http://qa2.vytrack.com/call/
-//     *
-//     * @param tab
-//     * @param module
-//     */
-//    public void navigateToModule(String tab, String module) {
-//        String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
-//        String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
-//        try {
-//            BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
-//            WebElement tabElement = Driver.get().findElement(By.xpath(tabLocator));
-//            new Actions(Driver.get()).moveToElement(tabElement).pause(200).doubleClick(tabElement).build().perform();
-//        } catch (Exception e) {
-//            BrowserUtils.clickWithWait(By.xpath(tabLocator), 5);
-//        }
-//        try {
-//            BrowserUtils.waitForPresenceOfElement(By.xpath(moduleLocator), 5);
-//            BrowserUtils.waitForVisibility(By.xpath(moduleLocator), 5);
-//            BrowserUtils.scrollToElement(Driver.get().findElement(By.xpath(moduleLocator)));
-//            Driver.get().findElement(By.xpath(moduleLocator)).click();
-//        } catch (Exception e) {
-////            BrowserUtils.waitForStaleElement(Driver.get().findElement(By.xpath(moduleLocator)));
-//            BrowserUtils.clickWithTimeOut(Driver.get().findElement(By.xpath(moduleLocator)),  5);
-//        }
-//}
 
 }
